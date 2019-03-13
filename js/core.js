3ff5eade6c452475d1f59a72ba426a6c49eedbc0
Vladyslav Syzov <vsyz@telenor.dk>
2019-03-13 09:34:14 +0200

    add ignore files and base drag n drop

--- js/core.js
+++ js/core.js
@@ -1,19 +1,66 @@
-class Card{
-    constructor(cardSuit){
-        this.cardSuit = cardSuit;
-    }
+class Card {
+	constructor(cardSuit) {
+		this.cardSuit = cardSuit;
+	}
 }
 
-class Solitaire{
-    constructor() {
-        this.deck = [
-            new Card('card_ah'),
-        ];
-      }
-    
-      shuffle() {
-        alert("shuffling");
-      }
+class Solitaire {
+	constructor() {
+		this.deck = [
+			new Card('card_ah'),
+		];
+	}
+
+	shuffle() {
+		alert("shuffling");
+	}
 }
 
 let game = new Solitaire();
+
+var fn = function() {
+
+	var card = document.getElementsByClassName('card')[0];
+
+	card.onmousedown = function(e) {
+
+		var coords = getCoords(card);
+		var shiftX = e.pageX - coords.left;
+		var shiftY = e.pageY - coords.top;
+
+		card.style.position = 'absolute';
+		document.body.appendChild(card);
+		moveAt(e);
+
+		card.style.zIndex = 1000; // над другими элементами
+
+		function moveAt(e) {
+			card.style.left = e.pageX - shiftX + 'px';
+			card.style.top = e.pageY - shiftY + 'px';
+		}
+
+		document.onmousemove = function(e) {
+			moveAt(e);
+		};
+
+		card.onmouseup = function() {
+			document.onmousemove = null;
+			card.onmouseup = null;
+		};
+
+	};
+
+	card.ondragstart = function() {
+		return false;
+	};
+
+	function getCoords(elem) { // кроме IE8-
+		var box = elem.getBoundingClientRect();
+		return {
+			top: box.top + pageYOffset,
+			left: box.left + pageXOffset
+		};
+	}
+};
+
+document.addEventListener('DOMContentLoaded', fn, false);
